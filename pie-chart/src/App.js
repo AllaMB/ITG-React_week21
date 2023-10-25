a9d99da98171b66894ebc4858978c20d10625b5a
AllaMB <alla.boussiron@gmail.com>
2023-10-25 18:36:54 +0200

    finished the task by creating the pie chart

--- pie-chart/src/App.js
+++ pie-chart/src/App.js
@@ -2,26 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import { Chart } from "react-google-charts";
 
-
-export const data = [
-  ["Character", "Popularity"],
-  ["Iron Man", 50],
-  ["Spider-Man", 80],
-  ["Thor", 40],
-  ["Black Widow", 60],
-  ["Captain America", 70],
-  ["Hulk", 15],
-  ["Black Panther", 55],
-  ["Captain Marvel", 65],
+function App() {
+  const data = [
+  ['Character', 'Popularity'],
+  ['Iron Man', 50],
+  ['Spider-Man', 80],
+  ['Thor', 40],
+  ['Black Widow', 60],
+  ['Captain America', 70],
+  ['Hulk', 15],
+  ['Black Panther', 55],
+  ['Captain Marvel', 65],
 ];
-
-export const options = {
-  title: "Popularity of Marvel Characters",
-  colors: ["#ff0000", "#ff9900", "#ffff00", "#009900", "#0000ff", "#6600cc", "#ff33cc", "#ff9900"],
+const options = {
+  title: "Popularity of Marvel Characters by ME",
+  colors: [
+    "#ff0000",
+    "#ff9900",
+    "#ffff00", 
+    "#009900", 
+    "#0000ff", 
+    "#6600cc", 
+    "#ff33cc", 
+    "#ff9900",
+  ],
   is3D: true,
 };
 
-function MarvelCharacterChart() {
   return (
     <Chart
       chartType="PieChart"
@@ -34,6 +41,6 @@ function MarvelCharacterChart() {
   );
 }
 
-export { MarvelCharacterChart };
+export default App ;
 
 
